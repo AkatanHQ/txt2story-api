c2f19a94b9b2846fb37630258d250b8d0dc9b13f
maria <mariahendrikx@gmail.com>
2024-09-09 14:10:01 +0200

    pictures are now publicly available

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from app.api import api
 
 def create_app():
-    app = Flask(__name__)
-    
+    # Specify the static folder during the Flask app initialization
+    app = Flask(__name__, static_folder='../static', static_url_path='/static')
+
     # Register blueprints
     app.register_blueprint(api, url_prefix='/api')
-    
+
     return app
